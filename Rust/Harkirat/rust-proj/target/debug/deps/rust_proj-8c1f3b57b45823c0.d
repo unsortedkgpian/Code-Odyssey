22342555966e2045e3ddcbd262a76bba3dc90639
unsortedkgpian <theadityakumar2810@gmail.com>
2025-06-13 15:27:25 +0530

    added basic program

---
+++ Rust/Harkirat/rust-proj/target/debug/deps/rust_proj-8c1f3b57b45823c0.d
@@ -0,0 +1,5 @@
+/home/aditya/Documents/Code Odyssey/Rust/Harkirat/rust-proj/target/debug/deps/librust_proj-8c1f3b57b45823c0.rmeta: src/main.rs
+
+/home/aditya/Documents/Code Odyssey/Rust/Harkirat/rust-proj/target/debug/deps/rust_proj-8c1f3b57b45823c0.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/Harkirat/rust-proj/target/debug/rust-proj.d
@@ -0,0 +1,1 @@
+/home/aditya/Documents/Code\ Odyssey/Rust/Harkirat/rust-proj/target/debug/rust-proj: /home/aditya/Documents/Code\ Odyssey/Rust/Harkirat/rust-proj/src/main.rs
