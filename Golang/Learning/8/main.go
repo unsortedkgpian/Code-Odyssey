828f1355f30cdaed518fd127733d77090925b11b
unsortedkgpian <theadityakumar2810@gmail.com>
2025-07-04 13:22:28 +0530

    arrays

---
+++ Golang/Learning/8/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main(){
+	var nums [8]int // -> make all element 0
+	nums[0] =32
+	nums[2] =54
+	nums[3] = 45
+	nums[4] = 22
+	nums[1] =98
+
+	fmt.Println("the lenght of array is ",len(nums))
+	for i :=range 8{
+		fmt.Println(nums[i])
+	}
+	fmt.Println(nums)
+	// -> wrong println(nums)
+
+	var boolen [4]bool 
+	fmt.Println(boolen)
+
+	var stringa [4]string 
+	fmt.Println(stringa)
+}
