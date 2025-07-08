dd6fd61028f4d1e54de15a09bce672b940e2e903
unsortedkgpian <theadityakumar2810@gmail.com>
2025-07-08 12:08:23 +0530

    learing through guide

---
+++ Golang/An_introduction_to_programming_in_Go/1/main.go
@@ -0,0 +1,7 @@
+// Getting Started
+//Google made go 
+// Go is general purpose language working for all work 
+
+// Workflow -> Gather requriments -> Find a solution -> Write code and implement solution -> compile  the source code  into an excutable ->  Run  and test  the program  to make  it works
+
+// go version
