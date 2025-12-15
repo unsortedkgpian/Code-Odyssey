f9baf930a5f5316d1a4499f44d5b2af2a0b84910
unsortedkgpian <theadityakumar2810@gmail.com>
2025-12-15 23:50:56 +0530

    start

---
+++ Rust/stephen-grider/deck/src/main.rs
@@ -0,0 +1,28 @@
+#[derive(Debug)]
+struct Deck {// Name of Struct always capitalized
+    cards:Vec<String>,
+}
+
+// Derive is altributes tells the rust to impl some trait which is Debug
+
+fn main() {
+
+    // List of 'suits' - 'Spades', 'Clubs', 'Heart', 'Diamonds'
+    // List of values - 'ace', 'two', 'three', 'four', 
+    // Generating all type of list 
+    let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
+    let values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Jack", "Queen", "King"];
+
+    for suit in suits {
+        for value in values {
+            
+        }
+    }
+
+    let deck:Deck = Deck { cards : vec![] };
+    let _deck1 : Deck = Deck { cards : Vec::new() };
+
+    //println!("Hello, world!");
+    println!("Heres your deck {:?}", deck);
+    println!("Heres your deck {:#?}", deck);
+}
