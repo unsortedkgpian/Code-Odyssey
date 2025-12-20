51b08d8860a94be1863fd0d0f4b0c19fc256f109
unsortedkgpian <theadityakumar2810@gmail.com>
2025-12-20 17:54:15 +0530

    lifetime

--- Rust/stephen-grider/bank/src/main.rs
+++ Rust/stephen-grider/bank/src/main.rs
@@ -13,6 +13,18 @@ impl Account {
             holder,
         }
     }
+
+    fn deposit(&mut self, amount:&i32) {
+        self.balance +=amount;
+    }
+
+    fn withdrawals(&mut self , amount:&i32){
+        self.balance -= amount;
+    }
+
+    fn summary(&self) -> String {
+        format!("{} has a balance {}", self.holder, self.balance)
+    }
 }
 
 
@@ -25,6 +37,19 @@ impl Bank {
     fn new() -> Self{
         Bank{accounts:vec![]}
     }
+
+    fn add_account (&mut self, account :Account) {
+        self.accounts.push(account);
+    }
+    
+    fn total_balance(&self) -> i32 {
+        self.accounts.iter().map(|account | account.balance).sum()
+    }
+
+    fn summary(&self) -> Vec<String> {
+        self.accounts.iter().map(|account | account.summary()).collect::<Vec<String>>()
+    }
+
 }
 
 fn print_account(account:Account) -> Account {
@@ -49,15 +74,26 @@ fn Change_account(account: &mut Account){
     account.balance = 100;
 }
 
+fn make_and_print_account()  {// &Account { lifetime paramater
+    let account = Account::new(1, String::from("Aditya"));
+    println!("{:#?}",account);
+    //&account
+}
 
 fn main() {
     //println!("Hello, world!");
 
-    let bank = Bank::new();
+    let mut bank = Bank::new();
     let mut account = Account::new(1, String::from("Aditya"));
+    println!("{:#?}", account.summary());
 
+    let mut dd_account = Account::new(2, String::from("Ambani"));
+    dd_account.deposit(&43);
+    dd_account.withdrawals(&32);
+    bank.add_account(dd_account);
     //let other_bank = bank;
-
+    //
+    println!("Demo Account {:#?}", bank.summary());
     println!("New bank {:#?}", bank);
     //println!("New Account {:#?}", account);
     //account = print_account(account);
@@ -91,16 +127,16 @@ fn main() {
     //let list_of_account = vec![demo_account];
     //println!("{:#?}", demo_account);
     let other_bank = Bank::new();
-    let Acc = other_bank.accounts;
+    //let Acc = other_bank.accounts;
     //println!("{:#?}", other_bank.accounts);
 
     let mut d_account = Account::new(1, String::from("me"));
     let account_ref = &mut d_account;
     d_account.balance = 10;
-    Change_account(account_ref);
+    //Change_account(account_ref);
     println!("{:#?}", account);
 
-
+    println!("{:#?}", other_bank.summary());
 
 
 
