f4653a19fe289dadc75432279d16ac1b5c91f500
unsortedkgpian <theadityakumar2810@gmail.com>
2026-01-07 11:52:13 +0530

    basic

--- Rust/stephen-grider/generics/src/main.rs
+++ Rust/stephen-grider/generics/src/main.rs
@@ -1,7 +1,34 @@
-fn solve(a:f64, b:f64) ->f64 {
-    (a.powi(2) + b.powi(2)).sqrt()
+use num_traits::{ToPrimitive, Float};
+
+
+fn solve<T:Float, U:Float>(a:T, b:U) ->f64 {
+
+    let a_f64 =a.to_f64().unwrap();
+    let b_f64 = b.to_f64().unwrap();
+    (a_f64.powi(2) + b_f64.powi(2)).sqrt()
 }
+
+
+fn Solve<T:ToPrimitive, U:ToPrimitive>(a:T,b:U) -> f64{
+    let a_f64 = a.to_f64().unwrap();
+    let b_f64 = b.to_f64().unwrap();
+    (a_f64.powi(2) + b_f64.powi(2)).sqrt()
+}
+
+
+
 fn main() {
     //println!("Hello, world!");
-    println!("Sove : {} {} == {}",3,4,solve(3.0,4.0));
+    
+
+    let a:i32 = 3;
+    let b:f64 = 4.0;
+
+    let a_64 = a as f64;
+    let a_64 = a.to_f64();
+
+
+
+    println!("Sove :{}",Solve::<f32,f64>(a as f32,b));
+    println!("Solve: {}", Solve(a,b));
 }
