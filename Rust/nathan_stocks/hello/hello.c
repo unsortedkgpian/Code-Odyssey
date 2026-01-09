1bf602f8cd21c041483cdcfb826facad9f8917f0
unsortedkgpian <theadityakumar2810@gmail.com>
2026-01-09 12:18:28 +0530

    setup

---
+++ Rust/nathan_stocks/hello/hello.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(){
+  int enigma;
+  printf("%d\n",  enigma);
+}
---
+++ Rust/nathan_stocks/hello/src/main.rs
@@ -0,0 +1,45 @@
+fn main() {
+    let bunnies:i32 =3;
+    println!("Hello, world!");
+
+    let(bunnies, carrots) =(8,50);
+    println!("Bunnies :{} Carrots:{} ",bunnies,carrots);
+
+    let mut deco = 10;
+
+    // Scope
+    let x1 = 10;
+    {
+        let y1 = 32;
+        println!("{}, {}",x1,y1);
+    }
+
+    println!("{},",x1);
+
+    // shadow
+    let x2 = 32;
+    {
+        let x2 = 23;
+        println!("{}",x2);
+    }
+
+    println!("{}",x2);
+
+
+    let mut x_mut = 10;
+    let x_mut = 20;
+    // x_mut = 21; Error
+    
+
+    let enigma:i32;
+
+    if true{
+        enigma = 42;
+    } else{
+        enigma =32;
+    }
+
+    println!("enigma is {}", enigma);
+
+    
+}
